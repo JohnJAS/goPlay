e15062ceea6ae9c7ebaddd0f9d1f6a1cd145c742
JohnJAS <sjjworkspace@gmail.com>
2020-03-31 16:37:22 +0800

    file

--- test/test.go
+++ test/test.go
@@ -7,5 +7,11 @@ import (
 func main() {
 	a := [...]int{1, 2, 3, 4, 5}
 
-	fmt.Println(a)
+	b := []int{1, 2, 3, 4, 5}
+
+	c := make([]int, 5, 10)
+
+	fmt.Println(len(a), cap(a))
+	fmt.Println(len(b), cap(b))
+	fmt.Println(len(c), cap(c))
 }
