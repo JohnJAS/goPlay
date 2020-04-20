ca212ae60b12ec3fb99e59f40e9ad3fe742739df
JohnJAS <sjjworkspace@gmail.com>
2020-04-20 16:09:21 +0800

    add more methods in fileoperator

--- fileOperator/fileOperator.go
+++ fileOperator/fileOperator.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
+
+	"github.com/gobuffalo/packr"
 )
 
 func main() {
-	data, err := ioutil.ReadFile("./fileOperator/autoUpgrade.json")
+	//data, err := ioutil.ReadFile("fileOperator/autoUpgrade.json")
+	//if err != nil {
+	//	fmt.Println("File reading error", err)
+	//	return
+	//}
+	//fmt.Println("Contents of file:", string(data))
+	//
+	//fptr := flag.String("fpath", "fileOperator/autoUpgrade.json", "file path to read from")
+	//flag.Parse()
+	//fmt.Println("value of fpath is", *fptr)
+
+	//still have question why binary file wasn't generated
+	box := packr.NewBox("../testBox")
+	json, err := box.FindString("autoUpgrade.json")
 	if err != nil {
-		fmt.Println("File reading error", err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Println("Contents of file:", string(data))
+	fmt.Println("Contents of file:", json)
 }
---
+++ fileOperator/go.mod
@@ -0,0 +1,11 @@
+module fileOperator
+
+go 1.13
+
+require (
+	github.com/gobuffalo/envy v1.9.0 // indirect
+	github.com/gobuffalo/packd v1.0.0 // indirect
+	github.com/gobuffalo/packr v1.30.1
+	github.com/rogpeppe/go-internal v1.5.2 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+)
