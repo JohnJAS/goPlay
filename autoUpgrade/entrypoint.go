8acd88bff389d67a2baf922b55d6e15bc6f4b8c9
root <root@localhost.localdomain>
2020-05-08 17:59:02 +0800

    add options

--- autoUpgrade/entrypoint.go
+++ autoUpgrade/entrypoint.go
@@ -17,6 +17,18 @@ var UpgradeLog string
 //LogFile is autoUpgrade logfile path
 var LogFilePath string
 
+var CURRENT_DIR string
+
+//upgrade step already run
+var UPGRADE_STEP int = 0
+//upgrade exec call count, init 1 for the first call
+var UPG_EXEC_CALL int = 1
+
+//Because the script may not be in the cluster, users must provide a node in the cluster. 
+var NODE_IN_CLUSTER string
+//upgrade work dictionary on the nodes in the cluster
+var WORK_DIR string
+
 func init() {
 	if SysType == "windows" {
 		TempFolder = os.Getenv("TEMP")
@@ -32,22 +44,41 @@ func init() {
 }
 
 func main() {
-	fmt.Println("autoUpgrade.sh transfer to autoUpgrade.go...")
-
-	fmt.Println(SysType)
-	fmt.Println(TempFolder)
-	fmt.Println(LogFilePath)
-
 	app := &cli.App{
-		Name: "greet",
-		Usage: "fight the loneliness!",
-		Action: func(c *cli.Context) error {
-			fmt.Println("Hello friend!")
-			return nil
+		Name: "autoUpgrade",
+    	Usage: "Upgrade CDF with one command! You can learn more about the auto upgrade through the official document.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "d",
+				Aliases: []string{"dir"},
+				Required: true,
+				Destination: &WORK_DIR,
+				Usage:   "The working directory to use on all cluster nodes.Ensure the directory is empty and the file system as enough space. If you are a non-root user on the nodes inside the cluster, make sure you have permission to this directory.\n",
+			},
+			&cli.StringFlag{
+				Name:    "n",
+				Aliases: []string{"node"},
+				Required: true,
+				Destination: &NODE_IN_CLUSTER,
+				Usage:   "IP address of any node inside the cluster.This parameter is mandatory.\n",
+			},
+			&cli.StringFlag{
+				Name:    "u",
+				Value:  "root",
+				Aliases: []string{"sysuser"},
+				Usage:   "The user for the SSH connection to the nodes inside the cluster. This user must have the permission to operate on the nodes inside the cluster. The configuration of the user must be done before running this script. This parameter is optional.",
+			},
+			&cli.StringFlag{
+				Name:    "o",
+				Aliases: []string{"options"},
+				Usage:   "Set the options needed for each version of upgrade. For a single version, the rule is shown below. [upgradeVersion1]:[option1]=[value1],[option2]=[value2] Different versions use '|' to distinguish with others. [upgradeVersion1]:[option]=[value]|[upgradeVersion2]:[option]=[value] This parameter is optional.\n",
+			},
 		},
 	}
 
 	err := app.Run(os.Args)
+	fmt.Println(WORK_DIR)
+	fmt.Println(NODE_IN_CLUSTER)
 	if err != nil {
 		log.Fatal(err)
 	}
