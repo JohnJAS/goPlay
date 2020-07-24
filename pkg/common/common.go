78c8ff8dd1f913b367cffdb86a2657966a084956
JiaJun Sheng <jiajun.sheng@microfocus.com>
2020-07-24 10:56:00 +0800

    add more into pkg

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -2,3 +2,9 @@ package common
 
 const KubeSystemNameSpace = "kube-system"
 const CoreNameSpace = "core"
+
+//ENV value
+const K8SHome = "K8S_HOME"
+
+//Time Format
+const TIMESTAMP = "20060102150405"
--- upgradePreCheck/upgradePreCheck.go
+++ upgradePreCheck/upgradePreCheck.go
@@ -4,12 +4,19 @@ import (
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
+	"path/filepath"
+	"time"
 
+	cdfCommon "github.com/JohnJAS/goPlay/pkg/common"
 	cdflog "github.com/JohnJAS/goPlay/pkg/log"
+
 )
 
 //program env
 var currentDir string
+
+var k8sHome string
+
 //decouple with autoUpgrade json, currently I think it's better
 var upgradeChain = []string{
 	"000000",
@@ -32,6 +39,11 @@ func init() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	k8sHome = os.Getenv(cdfCommon.K8SHome)
+	if k8sHome == "" || k8sHome == nil {
+		log.Fatal("failed to get the value of K8S_HOME")
+	}
 }
 
 func main() {
@@ -39,6 +51,10 @@ func main() {
 	os.Args = append(os.Args, "202005")
 	os.Args = append(os.Args, "-t")
 	os.Args = append(os.Args, "202011")
+
+	var logger *log.Logger
+
+
 
 	app := &cli.App{
 		Name:            "upgradePreCheck",
@@ -86,6 +102,14 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func startLog() (logger *log.Logger, err error){
+	path := filepath.Join(k8sHome, "log","scripts", "upgradePreCheck-"+time.Now().UTC().Format(cdfCommon.TIMESTAMP)+".log")
+
+
+
+	return
 }
 
 func preCheck(c *cli.Context) (err error) {
