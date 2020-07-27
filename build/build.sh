87b535deb8377b345d9a30df08b90701ddebf7e9
JohnJAS <sjjworkspace@gmail.com>
2020-07-27 23:32:07 +0800

    remove useless code

--- build/build.sh
+++ build/build.sh
@@ -4,8 +4,6 @@ CURRENT_DIR=${PWD}
 
 WORKSPACE=${CURRENT_DIR}/../../goPlay
 OUTPUT_FOLDER=${CURRENT_DIR}/output
-OS=linux
-ARCH=amd64
 GO_VERSION=1.14.6
 
 while [[ ${#} -gt 0 ]]; do 
@@ -14,17 +12,9 @@ while [[ ${#} -gt 0 ]]; do
         shift;;
         -o|--output) OUTPUT_FOLDER=${1}
         shift;;
-        --os ) OS=${1}
-        shift;;
-        --arch ) ARCH=${1}
-        shift;;
         --go-verison ) GO_VERSION=${$1}
         shift;;
     esac
 done
-
-if [[ ! -d $OUTPUT_FOLDER ]] ; then
-    mkdir -p $OUTPUT_FOLDER
-fi
 
 docker run --name build-upgrade-v${GO_VERSION} --privileged --rm -v ${WORKSPACE}:/workspace -v ${OUTPUT_FOLDER}:/output --workdir /workspace golang:${GO_VERSION} bash build/buildInsideContainer.sh
