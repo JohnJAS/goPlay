10d78011ef0501070b724d30d0f2e0bebb8902d0
JohnJAS <sjjworkspace@gmail.com>
2020-07-27 23:26:02 +0800

    add windows version

--- build/buildInsideContainer.sh
+++ build/buildInsideContainer.sh
@@ -9,4 +9,11 @@ export http_proxy=web-proxy.us.softwaregrp.net:8080
 
 cd ${PWD}/autoUpgrade
 
+echo "Building autoUpgrade linux version..."
 GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${PWD}/build/output/autoUpgrade
+
+[[ $? == 0 ]] && echo "success" || echo "failed"
+
+echo "Building autoUpgrade windows version..."
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${PWD}/build/output/autoUpgrade.exe
+[[ $? == 0 ]] && echo "success" || echo "failed"
