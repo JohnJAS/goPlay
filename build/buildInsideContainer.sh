ed7ad6a364f4bf4734c62de7d97ab1cb6f508f9b
JohnJAS <sjjworkspace@gmail.com>
2020-07-27 23:28:30 +0800

    adjust output dir

--- build/buildInsideContainer.sh
+++ build/buildInsideContainer.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo ${PWD}
+CURRENT_DIR=${PWD}
 
 go env -w GOPROXY=https://goproxy.cn/
 
 export https_proxy=web-proxy.us.softwaregrp.net:8080
 export http_proxy=web-proxy.us.softwaregrp.net:8080
 
-cd ${PWD}/autoUpgrade
+cd ${CURRENT_DIR}/autoUpgrade
 
 echo "Building autoUpgrade linux version..."
-GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${PWD}/build/output/autoUpgrade
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${CURRENT_DIR}/build/output/autoUpgrade
 
 [[ $? == 0 ]] && echo "success" || echo "failed"
 
 echo "Building autoUpgrade windows version..."
-GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${PWD}/build/output/autoUpgrade.exe
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${CURRENT_DIR}/build/output/autoUpgrade.exe
 [[ $? == 0 ]] && echo "success" || echo "failed"
