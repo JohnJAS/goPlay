810260ac1d6c68e286a2926c770fc6acf911aeff
JohnJAS <sjjworkspace@gmail.com>
2020-07-27 23:17:23 +0800

    adjust docker build command

--- build/buildInsideContainer.sh
+++ build/buildInsideContainer.sh
@@ -9,4 +9,4 @@ export http_proxy=web-proxy.us.softwaregrp.net:8080
 
 cd ${PWD}/autoUpgrade
 
-GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" ${PWD}/build/output/autoUpgrade
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${PWD}/build/output/autoUpgrade
