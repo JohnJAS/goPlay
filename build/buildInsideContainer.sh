cc4af81276de0671b616c2caa34ad8ac4d38f841
JohnJAS <sjjworkspace@gmail.com>
2020-07-27 23:13:06 +0800

    add proxy

--- build/buildInsideContainer.sh
+++ build/buildInsideContainer.sh
@@ -2,6 +2,11 @@
 
 echo ${PWD}
 
+go env -w GOPROXY=https://goproxy.cn/
+
+export https_proxy=web-proxy.us.softwaregrp.net:8080
+export http_proxy=web-proxy.us.softwaregrp.net:8080
+
 cd ${PWD}/autoUpgrade
 
 GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" ${PWD}/build/output/autoUpgrade
