c2042f5c5ec6df07e7ce01b71d85451cedb92fb2
JiaJun Sheng <jiajun.sheng@microfocus.com>
2020-07-28 15:30:57 +0800

    remove set output folder

--- build/buildCLI.sh
+++ build/buildCLI.sh
@@ -6,14 +6,11 @@ export http_proxy=web-proxy.us.softwaregrp.net:8080
 CURRENT_DIR=${PWD}
 
 WORKSPACE=${CURRENT_DIR}/../../goPlay
-OUTPUT_FOLDER=${CURRENT_DIR}/output
 GO_VERSION=1.14.6
 
 while [[ ${#} -gt 0 ]]; do
     case "$1" in
         -w|--workspace ) WORKSPACE=${1}
-        shift;;
-        -o|--output) OUTPUT_FOLDER=${1}
         shift;;
         --go-verison ) GO_VERSION=${1}
         shift;;
@@ -30,6 +27,6 @@ docker pull golang:${GO_VERSION}
 #=====================================================================================================#
 echo "Starting to compile CDF upgrade binaries..."
 
-docker run --name build-upgrade-v${GO_VERSION} --privileged --rm -e "OUTPUT_FOLDER=${OUTPUT_FOLDER}" -v ${WORKSPACE}:/workspace -v ${OUTPUT_FOLDER}:/output --workdir /workspace golang:${GO_VERSION} bash build/buildInsideContainer.sh
+docker run --name build-upgrade-v${GO_VERSION} --privileged --rm -v ${WORKSPACE}:/workspace -v ${OUTPUT_FOLDER}:/output --workdir /workspace golang:${GO_VERSION} bash build/buildInsideContainer.sh
 
 [[ $? != 0 ]] && "Failed to finish building CDF upgrade binaries" && exit 1
--- build/buildInsideContainer.sh
+++ build/buildInsideContainer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 CURRENT_DIR=${PWD}
-[[ -z $OUTPUT_FOLDER ]] && OUTPUT_FOLDER=${CURRENT_DIR}/build/output
 
 git config --global url."git@github.com:".insteadOf "https://github.com/"
 go env -w GOPROXY=https://goproxy.cn/
@@ -14,17 +13,17 @@ export http_proxy=web-proxy.us.softwaregrp.net:8080
 cd ${CURRENT_DIR}/autoUpgrade
 
 echo "Building autoUpgrade linux version..."
-GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o $OUTPUT_FOLDER/autoUpgrade/autoUpgrade
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o ${CURRENT_DIR}/build/output/autoUpgrade/autoUpgrade
 
 [[ $? == 0 ]] && echo "success" || echo "failed"
 
 echo "Building autoUpgrade windows version..."
-GO111MODULE=on CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w" -o $OUTPUT_FOLDER/autoUpgrade/autoUpgrade.exe
+GO111MODULE=on CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w" -o ${CURRENT_DIR}/build/output/autoUpgrade/autoUpgrade.exe
 [[ $? == 0 ]] && echo "success" || echo "failed"
 
 #===============================================================================================================================#
 cd ${CURRENT_DIR}/upgradePreCheck
 
 echo "Building upgradePreCheck only linux version..."
-GO111MODULE=on CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w" -o $OUTPUT_FOLDER/upgradePreCheck/upgradePreCheck
+GO111MODULE=on CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w" -o ${CURRENT_DIR}/build/output/upgradePreCheck/upgradePreCheck
 [[ $? == 0 ]] && echo "success" || echo "failed"
