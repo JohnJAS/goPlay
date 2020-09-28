ed7612894dd3949b271133944660929b43aa1ef5
Joseph Sheng <jiajun.sheng@microfocus.com>
2020-09-28 16:08:48 +0800

    fix example code

--- gobook-code/chapter5/c5_2_4a.go
+++ gobook-code/chapter5/c5_2_4a.go
@@ -6,7 +6,11 @@ package main
 
 import (
 	"fmt"
-	"sync"
+)
+
+//工作池的goroutine数目
+const (
+	NUMBER = 10
 )
 
 //工作任务
@@ -16,7 +20,7 @@ type task struct {
 	result chan<- int
 }
 
-//任务执行:计算begin到end的和
+//任务处理:计算begin到end的和
 //执行结果写入到结果chan result中
 func (t *task) do() {
 	sum := 0
@@ -27,28 +31,33 @@ func (t *task) do() {
 }
 
 func main() {
-	//创建任务通道
+	workers := NUMBER
+
+	//工作通道
 	taskchan := make(chan task, 10)
 
-	//创建结果通道
+	//结果通道
 	resultchan := make(chan int, 10)
 
-	//wait用于同步等待任务的执行
-	wait := &sync.WaitGroup{}
+	//worker信号通道
+	done := make(chan struct{}, 10)
 
-	//初始化task的goroutine,计算100个自然数之和
-	go InitTask(taskchan, resultchan, 100)
+	//初始化task的goroutine,计算1000个自然数之和
+	go InitTask(taskchan, resultchan, 1000)
 
-	//每个task启动一个goroutine处理，
-	go DistributeTask(taskchan, wait, resultchan)
+	//分发任务在NUMBER个goroutine 池
+	DistributeTask(taskchan, workers, done)
 
-	//通过结果通道获取结果并汇总
+	//获取各个goroutine处理完任务的通知，并关闭结果通道
+	go CloseResult(done, resultchan, workers)
+
+	//通过结果通道处理结果
 	sum := ProcessResult(resultchan)
 
 	fmt.Println("sum=", sum)
 }
 
-//构建task并写入到task通道
+//初始化待处理task chan
 func InitTask(taskchan chan<- task, r chan int, p int) {
 	qu := p / 10
 	mod := p % 10
@@ -75,22 +84,29 @@ func InitTask(taskchan chan<- task, r chan int, p int) {
 	close(taskchan)
 }
 
-//读取task chan ,每个task 一个worker goroutine处理
-//并等待每个task运行完，关闭结果通道
-func DistributeTask(taskchan <-chan task, wait *sync.WaitGroup, result chan int) {
+//读取task chan 分发到worker goroutine 处理，workers的总的数量是workers
+func DistributeTask(taskchan <-chan task, workers int, done chan struct{}) {
 
-	for v := range taskchan {
-		wait.Add(1)
-		go ProcessTask(v, wait)
+	for i := 0; i < workers; i++ {
+		go ProcessTask(taskchan, done)
 	}
-	wait.Wait()
-	close(result)
 }
 
-//工作goroutine处理具体工作，并将处理结构发送到结果通道
-func ProcessTask(t task, wait *sync.WaitGroup) {
-	t.do()
-	wait.Done()
+//工作goroutine处理具体工作，并将处理结构发送到结果chan
+func ProcessTask(taskchan <-chan task, done chan struct{}) {
+	for t := range taskchan {
+		t.do()
+	}
+	done <- struct{}{}
+}
+
+//通过done channel来同步等待所有工作goroutine的结束，然后关闭结果chan
+func CloseResult(done chan struct{}, resultchan chan int, workers int) {
+	for i := 0; i < workers; i++ {
+		<-done
+	}
+	close(done)
+	close(resultchan)
 }
 
 // 读取结果通道，汇总结果
