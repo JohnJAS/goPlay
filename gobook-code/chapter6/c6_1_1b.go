54282365e41dc3ca530fb0c957385a1f1033d28e
JohnJAS <sjjworkspace@gmail.com>
2020-10-26 23:42:25 +0800

    add commentms

--- gobook-code/chapter6/c6_1_1b.go
+++ gobook-code/chapter6/c6_1_1b.go
@@ -19,6 +19,11 @@ type Ita interface {
 	String() string
 }
 
+type Itb interface {
+	String() string
+	Test()
+}
+
 func (b B) String() string {
 	return b.b
 }
@@ -57,8 +62,18 @@ func main() {
 
 	//对于实参是未绑定具体变量的接口类型,reflect.TypeOf返回的是接口类型本身
 	//也就是接口的静态类型
+	//这里用Elem()的作用：由于interface{}是引用类型，通过 reflect.Elem() 方法获取这个指针指向的元素类型。这个获取过程被称为取元素，等效于对指针类型变量做了一个*操作
+	println(reflect.TypeOf(ita).Name())          //
+	println(reflect.TypeOf(ita).Kind().String()) //ptr
+
 	println(reflect.TypeOf(ita).Elem().Name())          //Ita
 	println(reflect.TypeOf(ita).Elem().Kind().String()) //interface
+
+	itbb := new(Itb)
+	println(reflect.TypeOf(itbb).Name())          //
+	println(reflect.TypeOf(itbb).Kind().String()) //ptr
+	println(reflect.TypeOf(itbb).Elem().Name())          //Itb
+	println(reflect.TypeOf(itbb).Elem().Kind().String()) //interface
 
 	//对于实参是绑定了具体变量的接口类型,reflect.TypeOf返回的是绑定的具体类型
 	//也就是接口的动态类型
