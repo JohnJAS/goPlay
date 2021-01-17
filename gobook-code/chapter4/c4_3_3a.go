e226abe7ad8c58e3f1bbab933b32f1f6d54d31dd
JohnJAS <sjjworkspace@gmail.com>
2021-01-17 23:15:23 +0800

    c433a i still have a question

--- gobook-code/chapter4/c4_3_3a.go
+++ gobook-code/chapter4/c4_3_3a.go
@@ -25,26 +25,31 @@ func main() {
 	var it Inter = st
 
 	//结果0x0表示空指针
-	fmt.Printf("%p\n", st)
-	fmt.Printf("%p\n", it)
+	fmt.Printf("%T %p %v\n", st, st, st)
+	fmt.Printf("%T %p %v\n", it, it, it)
+
+	if st == it {
+		fmt.Println(true)
+	}
 
 	if st == nil {
 		//此处st确实为nil
 		fmt.Println("st is nil")
 	}
 
-	//但这里it不为nil,是因为空接口有2个字段，一个是实例类型，一个是只想绑定实例的指针
-	if it != nil {
+	//但这里it不为nil,是因为空接口有2个字段，一个是实例类型，一个是指向绑定实例的指针
+	if it == nil {
+		fmt.Println("it is nil")
+	} else {
 		it.Pang()
 		//下面的语句会导致panic
 		//panic: value method main.St.Ping called using nil *St pointer
 		//方法转换为值接受者函数调用，第一个参数是St类型，由于*St是nil，无法获取指针所指的对象值，所以panic.
 		//it.Ping()
-	} else {
-		fmt.Println("it is nil")
 	}
 }
-//0x0
-//0x0
+
+//*main.St 0x0 <nil>
+//*main.St 0x0 <nil>
 //st is nil
 //pang
