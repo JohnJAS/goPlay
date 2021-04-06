23d0abb84df2e4011ddd6eac8feb9e3b937a438b
Joseph Sheng <jiajun.sheng@microfocus.com>
2021-04-06 15:43:48 +0800

    add comment

--- test/fileserver/server.go
+++ test/fileserver/server.go
@@ -11,6 +11,9 @@ const (
 
 func main() {
 	s := gin.Default()
+	//only server the files
+	s.Static("/dev0", ServePath)
+	//due to use the fs on server, it can get file list by using curl localhost:8099/dev/
 	s.StaticFS("/dev", http.Dir(ServePath))
 	s.Run(":8099")
 }
